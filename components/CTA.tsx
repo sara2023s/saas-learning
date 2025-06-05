da89c685134fa19f9f865093b6392cde84e12ebc
sara2023s <a.dasilvaus@gmail.com>
2025-06-06 09:40:32 +1200

    implement home page UI

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className="cta-section">
+        <div className="cta-badge">Start learning your way.</div>
+        <h2 className="text-3xl font-bold">
+            Build and Personalize Learning Companion
+        </h2>
+        <p>
+            Pick a name, subject, voice, & personality
+            - start learning through voice conversations that feel natural and fun.
+        </p>
+
+        <Image
+            src="images/cta.svg"
+            alt="cta"
+            height={232}
+            width={362}
+         />
+
+         <button className="btn-primary">
+            <Image
+                src="/icons/plus.svg"
+                alt="plus" 
+                width={12}
+                height={12}
+            />
+
+            <Link href="/companions/new">
+                <p>Build a New Companion</p>
+            </Link>
+
+         </button>
+        
+    </section>
   )
 }
 
